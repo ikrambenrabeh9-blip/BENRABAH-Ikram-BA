e6e89a248bb8932fdc308415c144f2729a93b9b5
ikrambenrabeh9-blip <ikrambenrabeh9@gmail.com>
2025-12-11 12:58:19 +0100

    Add files via upload

---
+++ ben rabah ikram biochimie appliquée e.py
@@ -0,0 +1,78 @@
+#benrabah ikram, biochimie appliquée, 6/12/2025 
+#bakhti riheb
+#Aina marwa 
+#senous chaimae 
+#sebdik imene 
+#Belaidi salim bilal 
+
+#Dans cette étape nous avons saisi les différentes séquences d’ADN dans une liste Python
+# méthode permet de stocker les données de manière structurée et de les manipuler facilement par la suite
+#Une saisie manuelle garantit également l’exactitude des données avant le début des analyses.
+
+import pandas as pd
+
+#Nous avons utilisé la bibliothèque Pandas afin de créer un DataFrame contenant les séquences.
+#Cette structure tabulaire est idéale pour l’analyse de données : elle permet d’ajouter des colonnes, d’effectuer des calculs, et de filtrer les informations rapidement
+#Convertir les séquences en tableau constitue une étape essentielle avant toute analyse bioinformatiq
+
+#Données: séquences ADN,longueur ,pourcentage de GC 
+data ={
+    "Séquences":["ATGCGTACGTA","GCTAGCTAGGCC","ATGCGCGTAAGT","TACGATCGTA","ATGAAAGGCTT","CGTACGTAGC","TTAACCGGAT"],
+    "longueur":[11,12,12,10,11,10,10],
+    "pourcentage GC":[50,66.67,58.33,40,45.45,60,50]
+    }
+#création d un DataFrame (tableau pandas )
+df=pd.DataFrame(data)
+print("**************création et affichage ****************")
+
+#affichage du tableau
+print("tableau des séquences ADN:")
+print(df,"\n\n")
+
+#opération sur tableau :
+print("**************opération **************")
+#1.) Sélectionner la colonne "longueur"
+Longuer=df["longueur"]
+print(Longuer)
+
+
+#2.)Filtrer les longueur supérieur à 10 
+print("************* Filtrage des longueur*************")
+# Filtrer les longueur supérieur à 10 
+filtered_df = df[df["longueur"] > 10 ]
+print(filtered_df,"\n\n")
+
+
+#3.)Calculer la moyenne du pourcentage de GC
+print("************* Calcul de la moyenne *************")
+# Calculer la moyenne du pourcentage de GC
+average_gc = df["pourcentage GC"].mean()
+print(f"pourcentage moyen de GC : {average_gc:.3f}%")
+print(df,"\n\n")
+
+
+#4).ajouter une nouvelle colonne avec calculs
+print("********ajouter une nouvelle colonne**********")
+#ajouter une nouvelle colonne "categorisée GC"
+df["categorisée GC"]=df["pourcentage GC"].apply(lambda x:"Riche"if x>55 else "moyen" if 45<=x<=55 else "faible" )
+print(df,"\n\n")
+
+
+
+#5).ajouter une colonne comptant les "G" Ajouter une nouvelle colonne avec des calculs
+df["Nombre de G"]=df["Séquences"].str.count ("G")
+print(df,"\n\n")
+
+# 6) Calculer l’écart-type du %GC et de la longueur des séquences
+
+print("===== Écart type =====")
+std_gc = df["pourcentage GC"].std()
+std_longueur = df["longueur"].std()
+print(f"7) Écart-type du %GC : {std_gc:.3f}")
+print(f"   Écart-type de la Longueur : {std_longueur:.3f}")
+print(df,"\n\n")
+
+
+#7) Sauvegarder le tableau final dans un fichier CSV
+df.to_csv("sequences_adn_final.csv", index=False, encoding='utf-8')
+print("8) Tableau final sauvegardé dans 'sequences_adn_final.csv'")
